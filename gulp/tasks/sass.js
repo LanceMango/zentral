2c93dfa8abceb717a049475ee4c33e36b8e08d8c
Lance <lance@smuepop.de>
2016-11-10 16:40:04 +0100

    gulp scss stylish error plumber

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,5 +1,6 @@
 var scsslint = require('gulp-scss-lint');
 var plumber = require('gulp-plumber');
+var scssLintStylish = require('gulp-scss-lint-stylish');
 
 module.exports = function (gulp, plugins, path, onError) {
     return function () {
@@ -7,15 +8,17 @@ module.exports = function (gulp, plugins, path, onError) {
             browsers: ['last 2 versions'],
         };
 
-        var plumberOptions = {
-            errorHandler: onError,
-        };
+        function handleError(err) {
+            console.log(err.toString());
+            this.emit('end');
+        }
 
         return gulp.src(path.SRC_SCSS)
-            .pipe(plugins.plumber(plumberOptions))
+            .pipe(plugins.plumber({errorHandler: handleError}))
             .pipe(plugins.sourcemaps.init())
             .pipe(scsslint({
-                'config': './gulp/config/scsslint.yml'
+                'config': './gulp/config/scsslint.yml',
+                customReport: scssLintStylish
             }))
             .pipe(plugins.sass())
             .pipe(plugins.autoprefixer(autoprefixerOptions))
